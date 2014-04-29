70d6bd16f8b0cd6a660b361978bd225aff2a4af9
evan <evan@sdicg.com>
2014-04-29 09:44:04 -0400

    getting ready for npm release

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function(chunk) {
-	(chunk = process.stdin.read()) &&
-		process.stdout.write(chunk.replace(/\\n/g, "\n").replace(/\\t/, "\t"));
+process.stdin.on('data', function(chunk) {
+	process.stdout.write(chunk.replace(/\\n/g, "\n").replace(/\\t/, "\t"));
 });
