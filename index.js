cafcf9ed56405091f1d90e349d3b10fa5db2ba37
evan <evan@sdicg.com>
2014-04-30 08:51:51 -0400

    fixing bug on multiple tabs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(chunk) {
-	process.stdout.write(chunk.replace(/\\n/g, "\n").replace(/\\t/, "\t"));
+	process.stdout.write(chunk.replace(/\\n/g, "\n").replace(/\\t/g, "\t"));
 });
